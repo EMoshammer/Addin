aa084ef18ce48ca609fb2429290a23a2cb5e213b
EMoshammer <73123550+EMoshammer@users.noreply.github.com>
2020-10-23 01:18:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -73,10 +73,11 @@ Office.onReady(function() {
       for(var i = 0; i < rng.formulas.length; i++) {
         var rng_slice = rng.formulas[i];
         for(var j = 0; j < rng_slice.length; j++) {
-		if (/^=(?:.*[ !])?OutData\(.*\)/i.test(rng_slice[j])) {
-			refreshCells2.push({i: i, j: j, val: rng_slice[j]});
-	  //var mtch = rng_slice[0].match(/^=(?:.*[ \+\-\*\/!])?OutData\((.*)$/i);
-		}
+		      //if (/^=(?:.*[ !])?OutData\(.*\)/i.test(rng_slice[j])) {
+		      if (/^=(?:.*[ \+\-\*\/!])?OutData\((.*)$/i.test(rng_slice[j])) {
+			      refreshCells2.push({i: i, j: j, val: rng_slice[j]});
+	          //var mtch = rng_slice[0].match(/^=(?:.*[ \+\-\*\/!])?OutData\((.*)$/i);
+		      }
           //if (mtch) {
           //  refreshCells2.push({i: i, j: j, val: rng_slice[j]}); //, args: mtch[1]});
           //}
