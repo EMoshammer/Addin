9f7c05835cef27b228f2c7332dfbf08faeb409c4
EMoshammer <73123550+EMoshammer@users.noreply.github.com>
2021-01-24 03:30:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -68,9 +68,6 @@ function LoadNextParam(refreshCells, iter) {
 Office.onReady(function() {
   
 	report = function(status, r) {
-    var toInsert = document.createElement("div");
-    toInsert.innerHTML = JSON.stringify(status) + '; ' + r.query + ': ' + JSON.stringify(r.value);
-    document.body.appendChild(toInsert);
 		
 		if (r.state == 'success') {
 		
@@ -82,8 +79,12 @@ Office.onReady(function() {
 				var j = r.j + r.offset;
 			}
 			
+    var toInsert = document.createElement("div");
+    toInsert.innerHTML = JSON.stringify(status) + '; ' + r.query + ': ' + JSON.stringify(r.value.data) + ' : ' + r.value.data.length + ' : ' + r.value.data[0].length;
+    document.body.appendChild(toInsert);
+			
 			//var rng = r.sht.getRangeByIndexes(i, j, r.value.data.length, r.value.data[0].length);
-			r.sht.getCell(i,j).getResizedRange(r.value.data.length, r.value.data[0].length).values = [[ 4 ]];
+			r.sht.getCell(i,j).values = r.value.data; //.getResizedRange(r.value.data.length, r.value.data[0].length)
 			//rng.values = r.value.data;
 
 			r.sht.getRange("C1").values = [[ 4 ]];
