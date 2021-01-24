3acb2d76a7b0304707da627e0209b67e237502da
EMoshammer <73123550+EMoshammer@users.noreply.github.com>
2021-01-24 03:20:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ Office.onReady(function() {
 			}
 			
 			//var rng = r.sht.getRangeByIndexes(i, j, r.value.data.length, r.value.data[0].length);
-			var rng = r.sht.getRangeByIndexes(6, 6, 1, 1);
+			var rng = r.sht.getCell(6,6).getResizedRange(1,1);
 			//rng.values = r.value.data;
 			rng.values = [[ 5 ]];
 			r.sht.getRange("C1").values = [[ 2 ]];
